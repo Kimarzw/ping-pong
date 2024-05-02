51d812f9b5c38beefb5ca35a42db4f3333d43be1
Kimarzw <167623740+Kimarzw@users.noreply.github.com>
2024-05-02 22:49:04 +0300

    игрок1
    v0.2

--- pinpong.py
+++ pinpong.py
@@ -4,7 +4,7 @@ from pygame import *
 
 
 img_back = '1.jpg'
-
+img_hero = '11.png'
 #игровая сцена
 win_width = 700
 win_height = 500
@@ -26,6 +26,7 @@ while game:
             game = False
 
 
+# класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
   # конструктор класса
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -40,11 +41,31 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+        
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+        
+        
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 20)
+bullets = sprite.Group()
 
       # сама игра: действия спрайтов, проверка правил игры, перерисовка
-    if not finish:
+if not finish:
         # обновляем фон
-        window.blit(background,(0,0))
+    window.blit(background,(0,0))
 
     display.update()
     clock.tick(FPS)
