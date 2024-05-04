eb1c2df81d79ea2bb9499be7ea9260b942f49822
Kimarzw <167623740+Kimarzw@users.noreply.github.com>
2024-05-04 14:22:59 +0300

    Update pinpong.py
    v1.0

--- pinpong.py
+++ pinpong.py
@@ -4,12 +4,16 @@ from pygame import *
 
 
 img_back = '1.jpg'
-img_hero = '11.png'
+
 #игровая сцена
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
+
+
+
+
 
 #флаги, отвечающие за состояние  игры
 game = True
@@ -26,46 +30,10 @@ while game:
             game = False
 
 
-# класс-родитель для других спрайтов
-class GameSprite(sprite.Sprite):
-  # конструктор класса
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        # Вызываем конструктор класса (Sprite):
-        sprite.Sprite.__init__(self)
-
-        # каждый спрайт должен хранить свойство image - изображение
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
-
-        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
- 
-  # метод, отрисовывающий героя на окне
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-
-        
-# класс главного игрока
-class Player(GameSprite):
-    # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-
-        
-        
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 20)
-bullets = sprite.Group()
-
       # сама игра: действия спрайтов, проверка правил игры, перерисовка
-if not finish:
+    if not finish:
         # обновляем фон
-    window.blit(background,(0,0))
+        window.blit(background,(0,0))
 
     display.update()
     clock.tick(FPS)
