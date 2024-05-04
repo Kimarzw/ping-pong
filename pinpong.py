e4a8d80d9216234d6293573f1ec71877515c7c13
Kimarzw <167623740+Kimarzw@users.noreply.github.com>
2024-05-04 15:28:00 +0300

    добавление игроков
    v1.1

--- pinpong.py
+++ pinpong.py
@@ -4,15 +4,14 @@ from pygame import *
 
 
 img_back = '1.jpg'
+img_hero = '11.png'
+img_boll = '111.png'
 
 #игровая сцена
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
-
-
-
 
 
 #флаги, отвечающие за состояние  игры
@@ -23,11 +22,75 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight,height))
+        self.speed = player_speed
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+# класс 1 и 2 игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+
+racket1 = Player(img_hero, 30,200,4,50,150)
+racket2 = Player(img_hero,520,200,4,50,150)
+ball = GameSprite(img_boll,200,200,4,50,50)
+
+speed_x = 3
+speed_y = 3
+
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    if finish != True:
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        
+
+
+
+      # сама игра: действия спрайтов, проверка правил игры, перерисовка
+    if not finish:
+        # обновляем фон
+        
+
+        
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    window.blit(background,(0,0))
+    display.update()
+    clock.tick(FPS)
 
 
       # сама игра: действия спрайтов, проверка правил игры, перерисовка
